169c20c33926713b3934923f349c85c71b3ebcba
quadbeats <33650185+quadbeats@users.noreply.github.com>
2023-04-07 21:41:14 +0900

    Create 2178.py
    BFS basic

---
+++ Samsung/2178.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+N ,M = map(int,input().split())
+
+board = [list(map(int,input())) for _ in range(N)]
+
+dx=[0,1,0,-1] # 우 하 좌 상
+dy=[1,0,-1,0]
+
+def BFS(x,y):
+
+    queue = deque()
+    queue.append((x,y))
+    while queue:
+        x,y = queue.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+
+            if 0<= nx <N and 0<= ny < M:
+                if board[nx][ny] == 1:
+                    board[nx][ny] = board[x][y] +1
+                    queue.append((nx,ny))
+
+
+    return board[N-1][M-1]
+
+ans = BFS(0,0)
+print(ans)
